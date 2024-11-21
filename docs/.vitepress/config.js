0cb32b142c8c341812841bc9ab569b2a554241df
WindyPear-Team <fireguo@flweb.cn>
2024-11-22 00:53:46 +0800

    更新 config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,25 +2,6 @@ export default {
     head: [
         ['link', { rel: 'icon', href: '/logo2.png' }],
     ],
-    sidebar: {
-      // 当用户位于 `guide` 目录时，会显示此侧边栏
-      '/bot/': [
-        {
-          text: 'bot文档',
-          items: [
-            { text: '概览', link: '/bot/' },
-            { text: 'B站解析', link: '/bot/bili' },
-            { text: '娶群友', link: '/bot/waifu' },
-            { text: 'MC查询', link: '/bot/mc' },
-            { text: '音乐下载', link: '/bot/mudl' },
-            { text: '积分系统', link: '/bot/credit' },
-            { text: 'AI聊天', link: '/bot/ai' },
-            { text: '娱乐功能', link: '/bot/games' },
-            { text: '游戏查询', link: '/bot/gameinfo' }
-          ]
-        }
-      ]
-    },
     title: '风梨团队文档',
     lastUpdated: true,
     description: '风梨团队',
@@ -41,12 +22,31 @@ export default {
               ]
             }
         ],
+        sidebar: {
+            // 当用户位于 `bot` 目录时，会显示此侧边栏
+            '/bot/': [
+                {
+                    text: 'bot文档',
+                    items: [
+                        { text: '概览', link: '/bot/' },
+                        { text: 'B站解析', link: '/bot/bili' },
+                        { text: '娶群友', link: '/bot/waifu' },
+                        { text: 'MC查询', link: '/bot/mc' },
+                        { text: '音乐下载', link: '/bot/mudl' },
+                        { text: '积分系统', link: '/bot/credit' },
+                        { text: 'AI聊天', link: '/bot/ai' },
+                        { text: '娱乐功能', link: '/bot/games' },
+                        { text: '游戏查询', link: '/bot/gameinfo' }
+                    ]
+                }
+            ]
+        },
         socialLinks: [
             { icon: "github", link: "https://github.com/WindyPear-Team" },
         ],
         footer: {
             message: '风梨团队',
-            copyright: 'Copyright © 2018-2024 <a href="https://www.flweb.cn/">WindyPear-Team</a>  版权所有'
+            copyright: 'Copyright © 2018-2024 <a href="https://www.flweb.cn/">WindyPear-Team</a> 版权所有'
         },
         lastUpdatedText: "最近更新时间",
         search: {
@@ -67,6 +67,6 @@ export default {
                 },
               },
             },
-          },
+        },
     },
 };
