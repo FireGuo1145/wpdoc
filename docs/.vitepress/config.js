df0ce7067af46e83680513a4a7a0dddb5c307bdc
WindyPear-Team <fireguo@flweb.cn>
2024-11-22 00:21:36 +0800

    更新 config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,35 +2,32 @@ export default {
     head: [
         ['link', { rel: 'icon', href: '/logo2.png' }],
     ],
-    title: 'MCJPG组织网站',
+    title: '风梨团队文档',
     lastUpdated: true,
-    description: 'Minecraft服务器集体宣传组织 (MSJPG)-一个致力于Minecraft技术交流和服务器宣传的新兴组织',
+    description: '风梨团队',
     lang: 'zh-CN',
     themeConfig: {
-        siteTitle: 'MCJPG组织', //站点标题
-        logo: "/logo2.png",
-        description: '一个Minecraft服务器宣传组织',//MCJPG
+        siteTitle: '风梨团队文档', //站点标题
+        logo: "//api.flweb.cn/logos/logo.webp",
+        description: '',
         nav: [
-            { text: "成员服", link: "/" },
-            { text: "规则", link: "/rules/" },
-            { text: "组织架构", link: "/structure/" },
-            { text: "状态监测", link: "https://status.mcjpg.org/" },
+            { text: "主页", link: "/" },
+            { text: "bot文档", link: "/bot/" },
+            { text: "组织开源项目", link: "/osp/" },
+            { text: "风梨网", link: "https://www.flweb.cn/" },
             {
               text: "友情链接",
               items: [
-                { text: "随风的个人网站", link: "https://zhuyuxuan.link/" },
-                { text: "金苹果论坛", link: "https://mcodw.com/" },
-                { text: "笨蛋开服指南", link: "https://yizhan.wiki/NitWikit/" },
-                { text: "风梨影视", link: "https://v.flweb.cn/" }
+                { text: "MCJPG", link: "https://mcjpg.org/" }
               ]
             }
         ],
         socialLinks: [
-            { icon: "github", link: "https://github.com/MineJPGcraft/MCJPG" },
+            { icon: "github", link: "https://github.com/WindyPear-Team" },
         ],
         footer: {
-            message: 'MCJPG服务器宣传组织',
-            copyright: 'Copyright © 2024-present <a href="https://mcjpg.org/">MCJPG</a>— —<a href="https://github.com/MineJPGcraft/MCJPG">我们的仓库</a>'
+            message: '风梨团队',
+            copyright: 'Copyright © 2018-2024 <a href="https://www.flweb.cn/">WindyPear-Team</a>  版权所有'
         },
         lastUpdatedText: "最近更新时间",
         search: {
